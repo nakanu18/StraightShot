57eaad4dfd2711bcb2fb1e1f79c4333d7714ee43
Alex de Vera <adever2@gmail.com>
2017-12-23 00:12:35 -0600

    v0.1 - Create basic header

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './Source/Components/Header';
 
 // Create a component
+const App = () => (
+    <Header headerText="Albums" />
+);
 
 // Render the component to the device
+AppRegistry.registerComponent('StraightShot', () => App);
