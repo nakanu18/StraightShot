978b21b838c65b55a6c41f5a8df8ee5f64d8a5f7
Alex de Vera <adever2@gmail.com>
2017-12-24 11:49:56 -0600

    v0.1 - Create CommonHeaderView.  Reorganize to components into subfolders.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Import a library to help create a component
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
-import ResultsListView from './Source/Components/ResultsListView';
+import CommonHeaderView from './Source/Components/Common/CommonHeaderView';
+import ResultsListView from './Source/Components/Results/ResultsListView';
 
 // Create a component
 const App = () => (
     <View>
+        <CommonHeaderView headerText='Results' />
         <ResultsListView />
     </View>
 );
