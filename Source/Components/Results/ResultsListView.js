6c97b0b788ec1d398fc1b00d17cfa6d12af51f29
Alex de Vera <adever2@gmail.com>
2018-01-06 21:48:03 -0600

    v0.1 - Create helper method to render Results

--- Source/Components/Results/ResultsListView.js
+++ Source/Components/Results/ResultsListView.js
@@ -13,14 +13,18 @@ class ResultsListView extends Component {
     componentWillMount() {
         // ALEX_TODO: build fake data
         this.setState({ results: [
-            { roundName: 'VEGAS 300', date: 'April 20', score: '258/300' },
-            { roundName: 'VEGAS 300', date: 'April 19', score: '272/300' },
-            { roundName: 'NFAA 300', date: 'April 1', score: '271/300' },
+            { roundId: 0, roundName: 'VEGAS 300', date: 'April 20', score: '258/300' },
+            { roundId: 1, roundName: 'VEGAS 300', date: 'April 19', score: '272/300' },
+            { roundId: 2, roundName: 'NFAA 300', date: 'April 1', score: '271/300' },
         ] });
     }
 
+    // Render methods
+
     renderResults() {
-        return this.state.results.map(result => <ResultsCell {...result} />);
+        return this.state.results.map(result =>
+            <ResultsCell key={result.roundId} {...result} />
+        );
     }
 
     render() {
