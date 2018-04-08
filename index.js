f288692010bcc5153451005918f58d8f5e3fa26c
Alex de Vera <adever2@gmail.com>
2018-04-07 21:59:39 -0500

    v01 - Add React Native Navigation library

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // Import a library to help create a component
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
-import CommonHeaderView from './Source/Components/Common/CommonHeaderView';
+import { Navigation } from 'react-native-navigation';
+
 import ResultsScreen from './Source/Components/Results/ResultsScreen';
 
-// Create a component
-const App = () => (
-    <View style={{ flex: 1 }}>
-        <CommonHeaderView headerText='Results' />
-        <ResultsScreen />
-    </View>
-);
+Navigation.registerComponent('ResultsScreen', () => ResultsScreen);
 
-// Render the component to the device
-AppRegistry.registerComponent('StraightShot', () => App);
+Navigation.startTabBasedApp({
+  tabs: [
+    {
+      label: 'One',
+      screen: 'ResultsScreen',
+      title: 'Results'
+    },
+    {
+      label: 'Two',
+      screen: 'ResultsScreen',
+      title: 'Screen Two'
+    }
+  ]
+});
